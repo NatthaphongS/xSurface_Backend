096cd8fe0ce6b0e2124d0519d3521a997ec90098
NatthaphongS <sinsupharoek@gmail.com>
2023-12-12 17:59:40 +0700

    fix multer

--- src/middleware/uploadmulter.js
+++ src/middleware/uploadmulter.js
@@ -1,23 +1,6 @@
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // console.log('------1');
-    cb(null, 'public');
-  },
-  filename: (req, file, cb) => {
-    // console.log('------2');
-    const split = file.originalname.split('.');
-    cb(
-      null,
-      '' +
-        Date.now() +
-        Math.round(Math.random() * 1000000) +
-        '.' +
-        split[split.length - 1]
-    );
-  },
-});
+const storage = multer.memoryStorage();
 
 const uploadmulter = multer({ storage: storage });
 
